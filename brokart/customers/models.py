1a101cf80d2cc18eeeb8f6205fac58083abdb50a
jishnu shankar <jsankar246@gmail.com>
2024-03-21 10:47:09 +0530

    data model design

--- brokart/customers/models.py
+++ brokart/customers/models.py
@@ -5,10 +5,10 @@ class Customer(models.Model):
     LIVE=1
     DELETE=0
     DELETE_CHOICES=((LIVE,'Live'),(DELETE,'Delete'))
-    name=models.Charfield(max_length=200)
+    name=models.CharField(max_length=200)
     address=models.TextField()
     user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='customer_profile')
-    phone=models.Charfield(max_length=10)
+    phone=models.CharField(max_length=10)
     delete_status=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
--- brokart/orders/models.py
+++ brokart/orders/models.py
@@ -3,14 +3,28 @@ from customers.models import Customer
 from products.models import Product
 # Create your models here.
 
-class Cart(models.Model):
+class Order(models.Model):
     LIVE=1
     DELETE=0
     DELETE_CHOICES=((LIVE,'Live'),(DELETE,'Delete'))
-    owner=models.ForeignKey(Customer,on_delete=models.SET_NULL,related_name='orders')
+    CART_STAGE=1
+    ORDER_CONFIRMED=1
+    ORDER_PROCESSED=2
+    ORDER_DELIVERED=3
+    ORDER_REJECTED=4
+    STATUS_CHOICE=((ORDER_PROCESSED,"ORDER_PROCESSED"),
+                   (ORDER_DELIVERED,"ORDER_DELIVERED"),
+                   (ORDER_REJECTED,"ORDER_REJECTED")
+                   )
+    order_status=models.IntegerField(choices=STATUS_CHOICE,default=CART_STAGE)
+
+    owner=models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True,related_name='orders')
     delete_status=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
-class CartItem(models.Model):
-    product=models.ForeignKey(Product)
+class OrderItem(models.Model):
+    product=models.ForeignKey(Product,related_name='added_carts',on_delete=models.SET_NULL,null=True)
+    quantity=models.IntegerField(default=1)
+    owner=models.ForeignKey(Order,on_delete=models.CASCADE,related_name='added_items')
+    
